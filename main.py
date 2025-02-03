53a2a8c87bb28fea12fe3e03c5e5275c7b1ca919
AiLeMao <mitchel_heijdel@hotmail.com>
2025-02-03 23:11:49 +0300

    sorted them letters in alphabethical order yolo

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_text(book_path)
     word_count = get_word_count(text)
-    count_letters(text)
+    letter_count = count_letters(text)
+    
+    
+    # Print the report command
+    print_report(book_path, word_count, letter_count)
+
+
+
 
 #take the text. convert to lowercase. count and pray it drops it out correctly due to being a library
 def count_letters(text):
@@ -15,8 +22,25 @@ def count_letters(text):
         else :
             letter_count[letter] = 1
 
-    print(letter_count)
+    return(letter_count)
 
+
+# Print the report function content
+def print_report(book_path, word_count, letter_count):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document\n")
+
+    # Convert the dictionary to a list of dictionaries
+    letters_list = [{"char": char, "num": count} for char, count in letter_count.items()]
+
+    # Sort the list by the "num" key in descending order
+    letters_list.sort(reverse=True, key=lambda x: x["num"])
+
+    # Print the sorted character counts
+    for item in letters_list:
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
 
 #takes "text" and splits it by strings aka words. length now gives the amount of strings aka words.
 def get_word_count(text):
