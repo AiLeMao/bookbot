d550b336b70fadbef713ee71e99a61d4d4d2ea58
AiLeMao <mitchel_heijdel@hotmail.com>
2025-02-03 22:57:34 +0300

    think i got the letter counting thing working

--- main.py
+++ main.py
@@ -2,17 +2,31 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_text(book_path)
     word_count = get_word_count(text)
-    print(f"{word_count} words found in this file!")
+    
 
+#take the text. convert to lowercase. count and pray it drops it out correctly due to being a library
+def count_letters(text):
+    letter_count = {}
+
+    for letter in text:
+        letter = letter.lower() #lowercase babyyyy
+        if letter in letter_count:
+            letter_count[letter] += 1
+        else :
+            letter_count[letter] = 1
+
+
+#takes "text" and splits it by strings aka words. length now gives the amount of strings aka words.
 def get_word_count(text):
     words = text.split()
     return len(words)
 
-
+#getting the book and writing it to "text"
 def get_text(path):
     with open(path) as f:
         return f.read()
     
+
 
 
 # Call the main function to execute the program
